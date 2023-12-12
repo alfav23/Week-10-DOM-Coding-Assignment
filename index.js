ae99d6339df7a7097ee35985bb48db84b9eecc6c
Alyssa Favorito <alyssafavorito2000@gmail.com>
2023-12-12 18:28:35 -0500

    stylistic changes

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-// create a variable to hold the table created in html with an id of the same name
+// variables to hold info from html for use later and to style elements
+let colorTable = document.getElementById("colorTable")
+colorTable.style.border = '1px solid white'
 let tableBody = document.getElementById("tableBody")
+tableBody.style.border ='1px solid white'
+let tableHeader1 = document.getElementById('tableHeader1')
+tableHeader1.style.border = '1px solid white'
+let tableHeader2 = document.getElementById('tableHeader2')
+tableHeader2.style.border = '1px solid white'
+// giving mainHeader from html a variable of same name styling to be white until event (mousemove)
 let mainHeader = document.getElementById('mainHeader')
+mainHeader.style.color = 'white'
+// creating an event listener that generates a random color for the header every time the mouse goes over it
 mainHeader.addEventListener("mousemove", () =>{
     mainHeader.style.color=
     `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`
 });
+
 console.log(mainHeader)
+// event listener to run buttonClicked function when submit button is clicked by user
 document.getElementById("submitButton").addEventListener("click", (event) => {
     // added a prevent default to stop the page from refreshing
     event.preventDefault()
@@ -16,10 +28,12 @@ function buttonClicked() {
     let newRow = document.createElement("tr");
 
     let nameInput = document.createElement("td");
+    nameInput.style.border='1px solid white'
     let nameValue = document.getElementById("name").value;
     nameInput.textContent = nameValue;
     
     let colorInput = document.createElement("td");
+    colorInput.style.border = '1px solid white'
     let colorValue = document.getElementById("color").value;
     colorInput.textContent = colorValue;
     
