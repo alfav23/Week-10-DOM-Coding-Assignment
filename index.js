a5a290a7a5eef848c0112331112471dba9e124e7
Alyssa Favorito <alyssafavorito2000@gmail.com>
2023-12-12 17:32:31 -0500

    fixing addEventListener and its function

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // create a variable to hold the table created in html with an id of the same name
-let dataTable = document.getElementById("dataTable")
-// start styling table which was created using bootstrap table
+let tableBody = document.getElementById("tableBody")
+document.getElementById("submitButton").addEventListener("click", (event) => {
+    // added a prevent default to stop the page from refreshing
+    event.preventDefault()
+    buttonClicked()})
 
-let newRow = document.createElement('tr')
-let nameInput = document.createElement('td')
-nameInput.append(document.getElementsByName('name'))
-console.log(nameInput)
-let colorInput = document.createElement('td')
-colorInput.append(document.getElementsByName('color'))
-console.log(colorInput)
-newRow.append(nameInput)
-newRow.append(colorInput)
-console.log(newRow)
-const submitButton = document.getElementById("submitButton")
-submitButton.addEventListener('click', () => {
-    dataTable.append(newRow)
-})
+// function to add input info to table when submit button is clicked
+function buttonClicked() {
+    let newRow = document.createElement("tr");
+
+    let nameInput = document.createElement("td");
+    let nameValue = document.getElementById("name").value;
+    nameInput.textContent = nameValue;
+    
+    let colorInput = document.createElement("td");
+    let colorValue = document.getElementById("color").value;
+    colorInput.textContent = colorValue;
+    
+                    // Adding a third cell to understand how to add a third cell
+                        // let thirdCell = document.createElement("td");
+                        // let thirdValue = "Third Cell";
+                        // thirdCell.textContent = thirdValue;
+
+    // appending inputs to row
+    newRow.append(nameInput);
+    newRow.append(colorInput);
+    // newRow.append(thirdCell);
+    
+    tableBody.append(newRow);
+};
